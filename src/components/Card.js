d791c2bc25023df31bae367cbad20704f59c4b78
jacobnathanson <jacobnath93@gmail.com>
2021-12-31 14:37:31 -0500

    card

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Card() {
+    return (
+        <div>
+            <div className="card--img"></div>
+            <div className="card--body">
+                <h5>stars</h5>
+                <h5>description</h5>
+                <h5>price</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Card
