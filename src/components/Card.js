9180b47c72e9313d282081b4e4060ac8df6e4533
jacobnathanson <jacobnath93@gmail.com>
2021-12-31 15:38:32 -0500

    card basic

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,17 @@ import React from "react";
 
 function Card() {
     return (
-        <div>
+
+        <div className="card--body">
             <div className="card--img"></div>
-            <div className="card--body">
-                <h5>stars</h5>
-                <h5>description</h5>
-                <h5>price</h5>
+            <div className="card--rating">
+                <img src="../assets/images/star-sm.png" alt="" className='star' />
+                <span>5.0</span>
+                <span>(6) • </span>
+                <span>USA</span>
             </div>
+        <p></p>
+            <h5>price</h5>
         </div>
     )
 }
