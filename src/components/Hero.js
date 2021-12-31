fae94b4a0b74ad39acebd05fdf4bdb267479a505
jacobnathanson <jacobnath93@gmail.com>
2021-12-31 14:18:10 -0500

    Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+function Hero() {
+    return (
+        <section className="hero">
+            <img className="hero--img" src="../assets/images/group-77.png" alt="group pictures" />
+            <h1 className="hero--header">Online experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+            </p>
+
+        </section>
+    )
+}
+
+export default Hero;
