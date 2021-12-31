c6855bee16e4a5864901f338cda1eefbe0e07dfa
jacobnathanson <jacobnath93@gmail.com>
2021-12-31 13:13:53 -0500

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+   return (
+    <nav>
+    <img className="nav--logo" src="../assets/images/airbnb-logo.png" alt="Logo"/>
+</nav>
+   );
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './App.css';
+import './style.css';
 
 ReactDOM.render(
   <React.StrictMode>
